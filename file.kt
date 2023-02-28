ea426325da0ee8994ab5c4fae7834199d77f1169
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-02-27 21:27:36 -0300

    Aula 2 - Introdução à linguagem Kotlin

--- file.kt
+++ file.kt
@@ -74,3 +74,39 @@ fun main() {
     var notaProvaGeral = 5 //0-10   
     println( notaProfessor >= 6 && notaProvaGeral >= 6 )    
 }
+
+//8) Inicialize 3 temperaturas e apresente em console se alguma delas é negativa
+
+fun main(){
+    var temp1:Double = -1.0
+    var temp2:Double = 3.0
+    var temp3:Double = 4.0
+        
+    if(temp1<0 || temp2<0 || temp3<0){
+    print("Uma das temperaturas é negativa")
+    }
+    else{
+    print("Todas as temperaturas são positivas")
+    }
+}
+
+//9) Inicialize as notas dos alunos, N1 e N2. Verifique se a média das duas é maior do que 6. Se sim, exiba uma mensagem indicando que o aluno foi aprovado. Senão Inicialize uma nova nota N3 e verifique se o aluno foi aprovado.
+
+fun main() {
+    val n1 = 7.5
+    val n2 = 8.0
+
+    val media = (n1 + n2) / 2
+
+    if (media >= 6) {
+        println("Aluno aprovado com média $media")
+    } else {
+        val n3 = 6.0
+        val mediaNova = (n1 + n2 + n3) / 3
+        if (mediaNova >= 6) {
+            println("Aluno aprovado com média $mediaNova")
+        } else {
+            println("Aluno reprovado com média $mediaNova")
+        }
+    }
+}
